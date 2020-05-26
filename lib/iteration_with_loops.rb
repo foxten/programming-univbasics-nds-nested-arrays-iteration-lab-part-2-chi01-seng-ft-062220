8fa789dd2da0d17d3435c19120e961e8f09c30de
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-26 01:45:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,14 +4,12 @@ count = 0
   while count < src.count do
     inner_count = 0 
       while inner_count < src[count].count do
-        min_temp = src[count].sort[-1]
+        min_temp = src[count].sort[0]
           if src[count][inner_count] < min_temp
             min_temp = src[count][inner_count]
           end
-          lowest_temperatures << min_temp
           inner_count +=1
       end
     count +=1
   end
 end
-lowest_temperatures 
