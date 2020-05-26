0993445030ce6c92f4395deaa453940ce7f9b45a
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-26 02:18:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,8 @@ count = 0
     inner_count = 0 
     min_temp = 0 
       while inner_count < src[count].count do
-          if src[count][inner_count] < src[count][inner_count]+1
-            min_temp = src[count][inner_count]+1
+          if src[count][inner_count] < src[count][inner_count+1]
+            min_temp = src[count][inner_count+1]
           end
           inner_count +=1
       end
